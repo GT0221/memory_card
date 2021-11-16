23f0bf5c2c4785a0e9668b3f86608acb35d3d1ba
Giovani <giotond25@gmail.com>
2021-11-16 13:12:58 -0500

    Complete App component logic and App stylesheet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
-
+import React, {useState, useEffect} from 'react';
+import Modal from './components/Modal';
+import Images from './components/Images';
+import ScoreBoard from './components/ScoreBoard';
 
 function App() {
+  const [clickedCharacter, setClickedCharacter] = useState([]);
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+
+  useEffect(() => {
+    const handleCharClick = (e) => {
+      const id = e.currentTarget.id;
+
+      if (!clickedCharacter.includes(id)) {
+        setClickedCharacter([...clickedCharacter, id]);
+        setScore(score + 1);
+        setBestScore(bestScore);
+      } else if (clickedCharacter.includes(id)) {
+        setClickedCharacter(() => []);
+        setScore(0);
+        setBestScore(score <= bestScore ? bestScore : score);
+      }
+    };
+
+    document
+      .querySelectorAll(".character")
+      .forEach((char) => char.addEventListener("click", handleCharClick));
+    return () => {
+      document
+        .querySelectorAll(".character")
+        .forEach((char) => char.removeEventListener("click", handleCharClick));
+    };
+  });
+
   return (
     <div className="App">
-      <h1>Hello from App</h1>
+      <h1>Street Fighter Memory Game</h1>
+      <Modal />
+      <ScoreBoard score={score} bestScore={bestScore}/>
+      <Images />
     </div>
   );
 }
